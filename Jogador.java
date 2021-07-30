9fc85306b36a0d27eb7080daa5d7a1095d1de84f
Jenyll <87767542+Jenyll@users.noreply.github.com>
2021-07-30 13:32:00 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,6 +1,6 @@
 package fbuni;
 
-//ESTA CLASSE FOI CRIADA AQUI APENAS COMO EXEMPLO. O ALUNO DEVE SUBSTITUI-LA POR OUTRA CLASSE CONFORME O PROJETO.
+
 public class Jogador extends DesenhoMovel {
 
 	public Jogador() {}
